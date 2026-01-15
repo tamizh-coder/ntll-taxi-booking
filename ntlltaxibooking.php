d178bba79f47182e389e2be60310d1f20c467378
Mohanraj Manogaran <73126752+tamizh-coder@users.noreply.github.com>
2026-01-15 22:09:57 +0530

    Update ntlltaxibooking.php

--- ntlltaxibooking.php
+++ ntlltaxibooking.php
@@ -5,7 +5,7 @@
 <title>NTLL Booking</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- <style>
+<!-- <styles>
     body {
         font-family: Arial, sans-serif;
         background: #f2f6ff;
